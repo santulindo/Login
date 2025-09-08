f2ded71cb81ee19cad53fcbed0136b4a995e608d
laura <danielamu1209@gmail.com>
2025-09-08 10:22:04 -0500

    login

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+from config import config
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('auth/login.html')
+
+@app.route('/register')
+def register():
+    return render_template('auth/register.html')
+
+if __name__=='__main__':
+    app.config.from_object(config['development'])
+    app.run()
